32c735fbb4d80f14b8cef3b414298090df33bda7
Dalsaetra <mattis.dalsaetra@gmail.com>
2025-02-01 19:16:15 -0500

    Deterministic stepper

--- python/src/neurons.py
+++ python/src/neurons.py
@@ -25,3 +25,28 @@ def IZ_Neuron_stepper_euler(states, params, I, dt):
     u = np.where(spike, u + d, u)
 
     return np.array([V, u, spike])
+
+def IZ_Neuron_stepper_euler_deterministic(states, params, I, dt):
+    # states: states_per_neuron x n_neurons, params: params_per_neuron x n_neurons
+    # states = [V, u]
+    # params = [k, a, b, d, C, Vr, Vt, Vpeak, c, delta_V]
+    # Vectorized version of IZ_Neuron.step_euler
+    n_neurons = states.shape[1]
+    # delta_V not used
+    k, a, b, d, C, Vr, Vt, Vpeak, c, delta_V, bias = params
+    V, u = states
+
+    spike = np.zeros(n_neurons, dtype=bool)
+
+    dV = (k * (V - Vr) * (V - Vt) - u + I + bias)/C
+    du = a * (b * (V - Vr) - u)
+
+    V += dt * dV
+    u += dt * du
+
+    spike = V >= Vpeak
+
+    V = np.where(spike, c, V)
+    u = np.where(spike, u + d, u)
+
+    return np.array([V, u, spike])
