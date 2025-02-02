8dbf24bff8e18729a155a65033de700be1141d90
Dalsaetra <mattis.dalsaetra@gmail.com>
2025-02-01 19:51:36 -0500

    Added weights to network overhead

--- python/src/network_overhead.py
+++ python/src/network_overhead.py
@@ -10,6 +10,9 @@ class NeuralNetwork:
         # Connectivity matrix
         # M[i, j] = k, k is the neuron index where the jth axon of the ith neuron ends up
         self.M = np.zeros((n_neurons, max_synapses), dtype=bool)
+        
+        # Weight matrix
+        self.W = np.zeros((n_neurons, max_synapses))
 
     # Initialize the connectivity matrix
     def set_connectivity(self, connectivity):
@@ -20,3 +23,16 @@ class NeuralNetwork:
         # Every row of M has max_synapses random unique indices from 0 to n_neurons-1
         for i in range(self.n_neurons):
             self.M[i] = np.random.choice(self.n_neurons, self.max_synapses, replace=replace)
+
+    # Initialize the weight matrix
+    def set_weights(self, weights):
+        self.W = weights
+
+    # Set weights to be random
+    def set_random_weights(self, distribution='uniform', **kwargs):
+        if distribution == 'uniform':
+            self.W = np.random.uniform(**kwargs)
+        elif distribution == 'normal':
+            self.W = np.random.normal(**kwargs)
+        else:
+            raise ValueError('Invalid distribution')
